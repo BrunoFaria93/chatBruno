b54c10bea02ce1a6b0b7003cf955ae87e7b422f0
Bruno Faria <soares_de_faria@hotmail.com>
2022-09-19 19:47:33 -0300

    pequenos ajustes

--- my-app/src/components/Chat/styles.js
+++ my-app/src/components/Chat/styles.js
@@ -4,9 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 90vh;
-  max-height: 90vh;
-  overflow: hidden;
+  height: 100vh;
   @media (min-width: 768px) {
   }
 
