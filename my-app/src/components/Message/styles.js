458799b197299de5da144bfa0b561ed7a0c9b20d
Bruno Faria <soares_de_faria@hotmail.com>
2022-09-19 21:37:06 -0300

    alteracoes visuais

--- my-app/src/components/Message/styles.js
+++ my-app/src/components/Message/styles.js
@@ -9,14 +9,14 @@ export const Line = styled.div`
   white-space: pre-wrap;
   &.me {
     > div {
-      background: linear-gradient(109.6deg, #dd5e89 13.4%, #f7bb97 80.2%);
+      background: #ff92a5;
     }
     justify-content: right;
   }
 `;
 
 export const Content = styled.div`
-  background: linear-gradient(109.6deg, #a8c0ff 13.4%, #3f2b96 70.2%);
+  background: #ebe0d0;
 
   border-radius: 10px;
   box-shadow: 0 1px 1px #dd5e89;
